efcc323d9c0df88079aed8e42bdfb964a876b1c2
cipher <asaforbrian18@gmail.com>
2024-05-25 14:09:24 +0300

    added the loginpage icon

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,6 +7,30 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+            // Message, app slogan
+
+            // email text field
+
+            // password textfield
+
+            // sign in button
+
+            // not a member? register now
+          ],
+        ),
+      ),
     );
   }
 }
