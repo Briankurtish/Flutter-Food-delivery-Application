65cb85c43271a6afe904434c66738e674ab577db
cipher <asaforbrian18@gmail.com>
2024-05-26 16:02:34 +0300

    created a tabBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:demo_tes/components/my_current_location.dart';
 import 'package:demo_tes/components/my_description_box.dart';
 import 'package:demo_tes/components/my_drawer.dart';
 import 'package:demo_tes/components/my_sliver_app_bar.dart';
+import 'package:demo_tes/components/my_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,23 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  // tab controller
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +36,9 @@ class _HomePageState extends State<HomePage> {
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
           MySliverAppBar(
-            title: const Text("title"),
+            title: MyTabBar(
+              tabController: _tabController,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
